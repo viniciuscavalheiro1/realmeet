54997b2c78017bbd50fb679b230e77701d91fe9c
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2023-07-05 15:39:00 -0300

    estrutura do projeto e db

---
+++ client/src/main/java/br/com/vinicius/realmeet/Client.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.realmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client.class, args);
+    }
+
+}
---
+++ service/src/main/java/br/com/vinicius/realmeet/RealmeetApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.realmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealmeetApplication.class, args);
+	}
+
+}
